cba97d9aebe6d8913657860c05b1f47c141e5211
Leonardo Henrique Petreca Costa <leopetrecca@gmail.com>
2022-04-18 15:25:00 -0300

    first commit

---
+++ .slicemachine/assets/slices/Uid/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Uid';
+
+export default {
+  title: 'slices/Uid'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"uid","items":[],"primary":{"title":[{"type":"heading1","text":"Revolutionize bleeding-edge bandwidth","spans":[]}],"content":[{"type":"paragraph","text":"Lorem commodo Lorem anim voluptate. Voluptate ipsum duis irure id esse cillum.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
